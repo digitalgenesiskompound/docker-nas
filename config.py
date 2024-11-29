220ca4fcc994022bf2811bf092e4d72c9fd082a7
digitalgenesiskompound <ngk@digitalgenesis.net>
2024-11-29 08:13:08 -0700

    Removed the default reverse proxy. Users will have to configure their own proxy.

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ load_dotenv()
 # Environment Variables
 VOLUME = os.getenv("VOLUME", "/volume")
 DATA_DIR = os.getenv("DATA_DIR", "/data")
-USERNAME = os.getenv("USERNAME", "default_user")
-
+HOST_PATH=os.getenv("HOST_PATH", "nas")
 # Data Directory and Credentials File
 CREDENTIALS_FILE = os.path.join(DATA_DIR, 'credentials.json')
